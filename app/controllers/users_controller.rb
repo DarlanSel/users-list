939083e8e13caeb7c79dd2baf9706cf158ec6bc2
Darlan Alaor Selzlein <sdarlanset@gmail.com>
2022-01-06 10:49:26 -0300

    Creating users view and download action.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def download_users
+    api_params = { nat: 'br', results: 30, inc: 'gender,name,email,picture', seed: 'giga' }
+
+    response = JSON(RestClient.get('https://randomuser.me/api/', content_type: :json, params: api_params))
+
+    response.deep_symbolize_keys!
+
+    User.bulk_create_from_response response[:results]
+
+    redirect_to root_path
+  end
+end
